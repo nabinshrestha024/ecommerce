0dca1a68b34476164f15fdb80411997a7958250d
hilubabz <utsargam44@gmail.com>
2025-12-11 10:47:30 +0545

    FE-chore(admin-routing): add page routing setup

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,9 +1,41 @@
-import { Input } from "./components/Input/Input";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./screens/dashboard";
+import { OrderManagement } from "./screens/order-management";
+import { Customer } from "./screens/customer";
+import { Category } from "./screens/category";
+import { ProductManagement } from "./screens/product-management";
+import { Transaction } from "./screens/transaction";
+import { Profile } from "./screens/profile";
+import { Login } from "./screens/login";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 export const App = () => {
   return (
-    <div>
-      <Input type="password" placeholder="Select Date" />
-    </div>
+    <>
+      <Navbar />
+      {/*simran*/}
+      <Sidebar />
+      {/*utsarga*/}
+      <Routes>
+        <Route path="/" element={<></>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/*utsarga*/}
+        <Route path="/order-management" element={<OrderManagement />} />
+        {/*simran*/}
+        <Route path="/customer" element={<Customer />} />
+        {/*pranawa*/}
+        <Route path="/category" element={<Category />} />
+        {/*pranawa*/}
+        <Route path="/product-management" element={<ProductManagement />} />
+        {/*amardeep*/}
+        <Route path="/transaction" element={<Transaction />} />
+        {/*simran*/}
+        <Route path="/profile" element={<Profile />} />
+        {/*amardeep*/}
+        <Route path="/login" element={<Login />} />
+        {/*utsarga*/}
+      </Routes>
+    </>
   );
 };
