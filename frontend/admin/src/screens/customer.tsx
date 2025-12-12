de0a40a51b5561dc26eabc2f7248d1b4abb0023d
pranawa0718 <pranawakc187@gmail.com>
2025-12-12 12:10:38 +0545

    FE-fix(customer): fix the width of parent div

--- frontend/admin/src/screens/customer.tsx
+++ frontend/admin/src/screens/customer.tsx
@@ -4,7 +4,7 @@ import { CustomerTable } from "@/components/Customer/CustomerTable";
 
 export const Customer = () => {
   return (
-    <div className=" pt-5 pb-[50px] pr-11 pl-5 flex flex-col gap-5">
+    <div className=" pt-5 pb-[50px] pr-11 pl-5 flex flex-col gap-5 w-full">
       <div className="flex gap-5">
         <CustomerDetails />
         <CustomerOverviewChart />
