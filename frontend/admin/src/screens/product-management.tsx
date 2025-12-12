a30c59cc2899c23742e3e5da3d5bbf6e4385795e
Amar <limbuamardeep18@gmail.com>
2025-12-12 13:32:40 +0545

    FE-feat(product-management-page): fix file structure

--- frontend/admin/src/screens/product-management.tsx
+++ frontend/admin/src/screens/product-management.tsx
@@ -1,9 +1,8 @@
 import { BasicDetails } from "@/components/ProductManagement/BasicDetails";
-import { SearchProduct } from "@/components/ProductManagement/SearchProdcut";
+import { Header } from "@/components/ProductManagement/Header";
 import { UploadProductDetails } from "@/components/ProductManagement/UploadProductDetails";
 import { Button } from "@/ui/button";
 import { Save } from "lucide-react";
-import { CirclePlus } from "lucide-react";
 import { useForm, FormProvider } from "react-hook-form";
 
 export const ProductManagement = () => {
@@ -13,28 +12,7 @@ export const ProductManagement = () => {
   };
   return (
     <div className="px-5 pt-5 pb-8">
-      <div className="flex justify-between items-center w-full px-5 pt-5 pb-8 gap-4">
-        <div className="font-bold text-[22px] leading-[100%] tracking-[0.5%] shrink-0">
-          Add New Product
-        </div>
-        <div className="flex-1 max-w-md">
-          <SearchProduct
-            placeholder="Search Products..."
-            className="w-full h-12"
-          />
-        </div>
-        <div className="flex flex-row gap-4 shrink-0">
-          <Button variant="default" className="h-12">
-            Publish Product
-          </Button>
-          <button className="flex flex-row justify-center items-center gap-2 px-4 py-1.5 h-12 rounded-md border font-bold tracking-[-2%] leading-[100%] text-[15px] border-gray-300 hover:bg-gray-50">
-            <Save size={16} /> Save to draft
-          </button>
-          <button className="flex justify-center items-center rounded-md border border-[#E5E7EB] h-12 w-12 hover:bg-gray-50">
-            <CirclePlus size={24} />
-          </button>
-        </div>
-      </div>
+      <Header />
       <FormProvider {...methods}>
         <form
           onSubmit={methods.handleSubmit(handleFormSubmit)}
