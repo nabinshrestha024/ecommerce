173727c9f8f54b0fbfcaf2a83f7ec665f43fca06
hilubabz <utsargam44@gmail.com>
2025-12-15 15:39:40 +0545

    FE-fix(client-navbar): fix the line on top issue

--- frontend/client/components/Navbar/components/NavCategories.tsx
+++ frontend/client/components/Navbar/components/NavCategories.tsx
@@ -41,7 +41,7 @@ export const NavCategories = () => {
 
             <span
               className={`
-            absolute left-1/2 -translate-x-1/2 -bottom-[1.5]
+            absolute left-1/2 -translate-x-1/2 -bottom-1.5
             h-[3px] w-[75%] rounded-xl bg-green-700
             origin-center
             transition-all duration-300 ease-out
