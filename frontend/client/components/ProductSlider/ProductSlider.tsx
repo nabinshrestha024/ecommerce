a7263743c9d08ee45ec2fa5c38968c13eaaaaa2a
Amar <limbuamardeep18@gmail.com>
2025-12-16 11:32:30 +0545

    FE-feat(client-carousel): wip carousel

--- frontend/client/components/ProductSlider/ProductSlider.tsx
+++ frontend/client/components/ProductSlider/ProductSlider.tsx
@@ -1,3 +1,36 @@
+import { Carousel } from "../carousel/Carousel";
+
 export const ProductSlider = () => {
-  return <div>Product Slider</div>;
+  return (
+    <Carousel
+      rootClassName="p-0 shadow-none"
+      contentClassName="p-0 border-0"
+      items={[
+        <img
+          key="electronics"
+          src="/ProductSlider/electronics.jpg"
+          alt="Electronics"
+          className="w-full h-64 object-cover rounded-lg"
+        />,
+        <img
+          key="groceries"
+          src="/ProductSlider/groceries.jpg"
+          alt="Groceries"
+          className="w-full h-64 object-cover rounded-lg"
+        />,
+        <img
+          key="shoes"
+          src="/ProductSlider/shoes.jpg"
+          alt="Shoes"
+          className="w-full h-64 object-cover rounded-lg"
+        />,
+        <img
+          key="sweater"
+          src="/ProductSlider/sweater.jpg"
+          alt="Sweater"
+          className="w-full h-64 object-cover rounded-lg"
+        />,
+      ]}
+    />
+  );
 };
