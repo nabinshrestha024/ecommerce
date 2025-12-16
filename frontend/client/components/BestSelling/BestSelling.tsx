e8584eca0015ac02c4fb8737f7be48d6940e02b5
Simran Maharjan <143006616+SimranMhzn@users.noreply.github.com>
2025-12-16 10:27:41 +0545

    FE-feat(best-selling): add routing on product

--- frontend/client/components/BestSelling/BestSelling.tsx
+++ frontend/client/components/BestSelling/BestSelling.tsx
@@ -1,3 +1,40 @@
+import { Button } from "@/ui/button";
+import Image from "next/image";
+import { bestSelling } from "./bestSelling.import";
+
 export const BestSelling = () => {
-  return <div>Best Selling</div>;
+  return (
+    <div>
+      <div className="w-full flex justify-between items-center">
+        <h1 className="font-bold text-xl">Best selling product</h1>
+        <Button
+          variant={"outline"}
+          className="rounded-2xl border border-black text-xs"
+        >
+          View All
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 grid-rows-2 gap-5 w-full mt-8 h-[400px]">
+        {bestSelling.map((card) => (
+          <div
+            key={card.id}
+            className="relative w-full h-full rounded-xl shadow overflow-hidden"
+            style={{
+              gridColumn: `${card.colStart} / span ${card.colSpan}`,
+              gridRow: `${card.rowStart} / span ${card.rowSpan}`,
+            }}
+          >
+            <a href={card.href}>
+              <Image
+                src={card.image}
+                alt="Image"
+                fill
+                className="object-cover"
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
