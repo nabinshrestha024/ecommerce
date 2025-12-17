e79673fea9267b3c698990e00c912f621d1fbe9c
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-12-17 14:12:31 +0545

    chore(db-configure): update UsersTable

--- backend/EcommerceProject/Database/01_Scripts/01_UsersTable.sql
+++ backend/EcommerceProject/Database/01_Scripts/01_UsersTable.sql
@@ -3,13 +3,14 @@ GO
 
 CREATE TABLE Users (
     UserId           INT IDENTITY(1,1) PRIMARY KEY,
-    Email            VARCHAR(320) NOT NULL UNIQUE,       
+    Email            VARCHAR(100) NOT NULL UNIQUE, 
+    FullName        VARCHAR(100) NOT NULL,      
     PasswordHash     VARCHAR(MAX) NULL,                   
     Status           SMALLINT NOT NULL DEFAULT 1,   -- Active, Inactive, Banned  
     ProfileImageUrl  VARCHAR(1024) NULL,    
     Phone            VARCHAR(20) NULL,
     Address          VARCHAR(500) NULL,
-    City             VARCHAR(200) NULL,
+    City             VARCHAR(100) NULL,
     Role             BIT NOT NULL DEFAULT 1, -- Admin = 0, Customer = 1
     IsActive         BIT NOT NULL DEFAULT 1,   
     CreatedAt        DATETIME2(3) NOT NULL DEFAULT SYSUTCDATETIME(),
