1d31d6bca65fe7541990b3de541b0fb7c8b82cb0
PRANAWA K.C <161146105+pranawa1819@users.noreply.github.com>
2025-12-17 10:45:26 +0545

    FE- feat/review: implement the ui of review

---
+++ frontend/client/components/Review/components/ReviewData.import.tsx
@@ -0,0 +1,50 @@
+export const ReviewData = [
+  {
+    id: 1,
+    name: "Emily R.",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+  {
+    id: 2,
+    name: "John D",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+  {
+    id: 3,
+    name: "Ahmed M.",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+  {
+    id: 4,
+    name: "Alex T",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+  {
+    id: 5,
+    name: "Priya R",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+  {
+    id: 6,
+    name: "David H",
+    image: "/Utsarga.jpg",
+    rating: 5,
+    message:
+      "Fast delivery and fantastic quality! The customer support team was quick to resolve my query. Dealport has earned a loyal customer.",
+  },
+];
