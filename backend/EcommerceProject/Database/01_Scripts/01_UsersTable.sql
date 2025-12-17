88b49c3a1fef4b8ef060ed7a5c2c8d5018030621
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-12-17 14:10:59 +0545

    chore(db-configure): update UsersTable

--- backend/EcommerceProject/Database/01_Scripts/01_UsersTable.sql
+++ backend/EcommerceProject/Database/01_Scripts/01_UsersTable.sql
@@ -5,8 +5,6 @@ CREATE TABLE Users (
     UserId           INT IDENTITY(1,1) PRIMARY KEY,
     Email            VARCHAR(320) NOT NULL UNIQUE,       
     PasswordHash     VARCHAR(MAX) NULL,                   
-    FirstName         VARCHAR(100) NOT NULL,
-    LastName          VARCHAR(100) NOT NULL,
     Status           SMALLINT NOT NULL DEFAULT 1,   -- Active, Inactive, Banned  
     ProfileImageUrl  VARCHAR(1024) NULL,    
     Phone            VARCHAR(20) NULL,
@@ -17,7 +15,6 @@ CREATE TABLE Users (
     CreatedAt        DATETIME2(3) NOT NULL DEFAULT SYSUTCDATETIME(),
     UpdatedAt        DATETIME2(3) NULL,
     DeletedAt        DATETIME2(3) NULL,
-    CONSTRAINT UQ_Users_Email UNIQUE (Email)
 );
 
 PRINT 'Table Users created successfully.';
