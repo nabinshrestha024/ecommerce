3c6d2b3a4aaead6a9b9910723aa668e391b248b6
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-12-18 14:30:32 +0545

    feat(inventory-vendor-procure): add stocking features for products.

--- backend/EcommerceProject/Repositories/Implementations/StockRepository.cs
+++ backend/EcommerceProject/Repositories/Implementations/StockRepository.cs
@@ -77,7 +77,7 @@ namespace EcommerceProject.Repositories.Implementations
                     commandType: CommandType.StoredProcedure
                 );
                 
-                return adjustment;    //git commit -m "feat(inventory-vendor-procure): add database
+                return adjustment;
             }
             catch (Exception ex)
             {
