f0c37071885c45064535d519ec1f1b54e29b3d40
Amar <limbuamardeep18@gmail.com>
2025-12-18 13:19:19 +0545

    FE-fix(productslider): fix slider images in slider

--- frontend/client/components/ProductSlider/ProductSlider.tsx
+++ frontend/client/components/ProductSlider/ProductSlider.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Carousel } from "../Carousel/Carousel";
 import Autoplay from "embla-carousel-autoplay";
 import { useMemo } from "react";
 import { Slides } from "./constants/Slides";
+import { Carousel } from "../carousel/Carousel";
+import Link from "next/link";
+
 export const ProductSlider = () => {
   const autoplay = useMemo(
     () => Autoplay({ delay: 6000, stopOnInteraction: true }),
@@ -12,27 +14,31 @@ export const ProductSlider = () => {
 
   return (
     <Carousel
-      rootClassName="relative w-full h-100 overflow-hidden"
-      contentClassName=""
+      rootClassName="relative w-full h-[400px] overflow-hidden"
       previousClassName="absolute left-4 top-1/2 -translate-y-1/2 bg-white text-gray-700 p-2 rounded-full shadow hover:bg-gray-100"
       nextClassName="absolute right-4 top-1/2 -translate-y-1/2 bg-white text-gray-700 p-2 rounded-full shadow hover:bg-gray-100"
       opts={{ loop: true }}
       plugins={[autoplay]}
       items={Slides.map((slide) => (
-        <div key={slide.key} className="realtive">
+        <div key={slide.key} className="relative w-full h-[400px]">
           <img
             src={slide.src}
             alt={slide.alt}
-            className="w-full h-100 object-cover overflow-hidden"
+            className="w-full h-full object-cover"
           />
-          {slide.showOverlay && (
-            <div className="absolute inset-0 flex items-center px-12 bg-black/30">
-              <div className="text-white max-w-md pl-12">
-                <h2 className="text-4xl font-bold">Big Sale</h2>
-                <p className="mt-2">Up to 50% off electronics</p>
-                <button className="mt-4 bg-white text-black px-6 py-2 rounded">
-                  Shop Now
-                </button>
+
+          {slide.overlay && (
+            <div className="absolute inset-0 flex  items-center px-12 bg-black/30">
+              <div className="ml-5 text-white max-w-md">
+                <h2 className="text-4xl font-bold">{slide.overlay.title}</h2>
+                <p className="mt-2">{slide.overlay.description}</p>
+
+                <Link
+                  href={slide.overlay.href}
+                  className="inline-block mt-4 bg-white text-black px-6 py-2 rounded"
+                >
+                  {slide.overlay.buttonText}
+                </Link>
               </div>
             </div>
           )}
