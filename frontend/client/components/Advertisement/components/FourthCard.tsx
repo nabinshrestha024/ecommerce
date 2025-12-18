f9856376138e71dd4c7c30bbf68c9039089970d8
Amar <limbuamardeep18@gmail.com>
2025-12-18 12:05:59 +0545

    FE-fix(advertisement-card): fix advertisement section images

--- frontend/client/components/Advertisement/components/FourthCard.tsx
+++ frontend/client/components/Advertisement/components/FourthCard.tsx
@@ -9,16 +9,20 @@ export const FourthCard = () => {
   ];
 
   return (
-    <div className="grid grid-cols-4 gap-2 pr-2 hover:cursor-pointer">
+    <div className="grid grid-cols-4 gap-2 pr-2">
       {advertisements.map((ad, index) => (
-        <div key={index}>
-          <Image
-            src={ad.src}
-            alt={ad.alt}
-            width={220}
-            height={192}
-            className="w-full h-48 object-fit rounded-xl shadow-[0px_1px_3px_0px_rgba(0,0,0,0.2)]"
-          />
+        <div
+          key={index}
+          className="flex justify-center items-center rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300"
+        >
+          <div className="relative w-[327px] h-[190px]">
+            <Image
+              src={ad.src}
+              alt={ad.alt}
+              fill
+              className=" object-cover rounded-xl shadow-[0px_1px_3px_0px_rgba(0,0,0,0.2)]"
+            />
+          </div>
         </div>
       ))}
     </div>
---
+++ frontend/client/components/Advertisement/constants/SecondCardContent.ts
@@ -0,0 +1,27 @@
+export type SecondCardItem = {
+  id: number;
+  src: string;
+  alt: string;
+};
+export const SecondCardContent = [
+  {
+    id: 1,
+    src: "/advertisement/headphone.png",
+    alt: "Gaming accessory 1",
+  },
+  {
+    id: 2,
+    src: "/advertisement/mouse.png",
+    alt: "Gaming accessory 2",
+  },
+  {
+    id: 3,
+    src: "/advertisement/controller.png",
+    alt: "Gaming accessory 3",
+  },
+  {
+    id: 4,
+    src: "/advertisement/chair.png",
+    alt: "Gaming accessory 4",
+  },
+];
