b39bf5e41c048800de85670f918c1e37793e3dc9
Amar <limbuamardeep18@gmail.com>
2025-12-19 15:48:35 +0545

    FE-fix(vendor): add overflow handling to vendor and profile form

--- frontend/admin/src/components/vendor/StatCard.tsx
+++ frontend/admin/src/components/vendor/StatCard.tsx
@@ -14,9 +14,7 @@ const StatCard = ({
     cardClassName="flex flex-col px-3 py-3 border border-[#D1D5DB] rounded-sm shadow-none"
   >
     <div className="flex flex-col gap-2">
-      <div className="text-center text-[18px] font-bold text-[#023337]">
-        {value ?? 0}
-      </div>
+      <div className="text-center text-[18px] font-bold ">{value ?? 0}</div>
       <div className="text-center text-[14px] font-normal" style={{ color }}>
         {label}
       </div>
