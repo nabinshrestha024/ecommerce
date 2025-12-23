f9b9e2cc29b50b248960b553844bf7f63ce3aab5
ritika-mhrjn <ritika2021mhrjn@gmail.com>
2025-12-23 15:15:42 +0545

    BE-feat(authorization): add authorization

--- backend/EcommerceProject/Controllers/v1/Category/CategoriesController.cs
+++ backend/EcommerceProject/Controllers/v1/Category/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using EcommerceProject.Models.DTOs.Category;
 using EcommerceProject.Models.DTOs.Common;
 using EcommerceProject.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,7 @@ namespace EcommerceProject.Controllers.v1.Category
 {
     [ApiController]
     [Route("v1/categories")]
+    [Authorize(Roles = "Admin, Customer")]
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _service;
