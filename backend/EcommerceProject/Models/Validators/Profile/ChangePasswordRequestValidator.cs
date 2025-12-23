86d6a5c5eb79615f681438518122090c8725b7a0
Aadarsha Khadka <aadar.kdk@gmail.com>
2025-12-23 10:48:34 +0545

    BE-feat(customer-profile): add complete customer profile module

--- backend/EcommerceProject/Models/Validators/Profile/ChangePasswordRequestValidator.cs
+++ backend/EcommerceProject/Models/Validators/Profile/ChangePasswordRequestValidator.cs
@@ -14,16 +14,6 @@ public class ChangePasswordRequestValidator : AbstractValidator<ChangePasswordRe
         RuleFor(x => x.NewPassword)
             .NotEmpty()
             .WithMessage("New password is required.");
-            //.MinimumLength(8)
-            //.WithMessage("New password must be at least 8 characters long.")
-            //.Matches(@"[A-Z]+")
-            //.WithMessage("New password must contain at least one uppercase letter.")
-            //.Matches(@"[a-z]+")
-            //.WithMessage("New password must contain at least one lowercase letter.")
-            //.Matches(@"[0-9]+")
-            //.WithMessage("New password must contain at least one digit.")
-            //.Matches(@"[\W_]+")
-            //.WithMessage("New password must contain at least one special character.");
     }  
 }
 }
