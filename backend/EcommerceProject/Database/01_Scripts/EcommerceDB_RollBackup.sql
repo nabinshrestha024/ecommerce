46f1dbf187068286e3635fb24428701f93d25106
ritika-mhrjn <ritika2021mhrjn@gmail.com>
2025-12-31 16:36:53 +0545

    BE-feat(variant): change the backup folder path

--- backend/EcommerceProject/Database/01_Scripts/EcommerceDB_RollBackup.sql
+++ backend/EcommerceProject/Database/01_Scripts/EcommerceDB_RollBackup.sql
@@ -1,19 +1,20 @@
 USE master;
 GO
 
--- force close all active connections
+-- 1. Force close all active connections
 ALTER DATABASE [EcommerceDB] 
 SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 GO
 
--- execute the restore
--- replace '20251231_1351' with the actual timestamp on your file in C:\Backups
+-- 2. Execute the restore
+-- REMOVED THE DOUBLE QUOTES HERE:
 RESTORE DATABASE [EcommerceDB] 
-FROM DISK = 'C:\Backups\EcommerceDB_Full_20251231_1351.bak'  -- change as required
-WITH REPLACE;
+FROM DISK = 'C:\Users\NITRO\Desktop\Backups\EcommerceDB_Full_20251231_1450.bak' 
+WITH REPLACE,
+     STATS = 5; -- Provides progress updates every 5%
 GO
 
--- bring the database back to normal mode
+-- 3. Bring the database back to normal mode
 ALTER DATABASE [EcommerceDB] 
 SET MULTI_USER;
 GO
