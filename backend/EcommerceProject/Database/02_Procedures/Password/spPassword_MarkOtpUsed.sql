1afee77d26d6026e216a6ea79330abb93176f072
Acer <Acer@DESKTOP-GOCI5KV>
2025-12-31 14:06:32 +0545

    BE-feat(updateuser):implement store procedure of otp

---
+++ backend/EcommerceProject/Database/02_Procedures/Password/spPassword_MarkOtpUsed.sql
@@ -0,0 +1,11 @@
+﻿USE [EcommerceDB];
+GO
+
+CREATE OR ALTER PROCEDURE spPassword_MarkOtpUsed
+    @OtpId INT
+AS
+BEGIN
+    UPDATE PasswordResetOtps
+    SET IsUsed = 1
+    WHERE OtpId = @OtpId
+END
