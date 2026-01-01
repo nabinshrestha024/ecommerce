d912ad7e6b39b2d79644e8de9e5b71a35f56e6de
Utsarga Manandhar <utsargam44@gmail.com>
2026-01-01 14:14:29 +0545

    FE-fix(client-register): add date of birth and gender field in register

--- frontend/client/ui/input.tsx
+++ frontend/client/ui/input.tsx
@@ -6,7 +6,33 @@ import { Eye, EyeOff } from "lucide-react";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, type, ...props }, ref) => {
-    return (
+    const [passwordVisible, setPasswordVisible] = React.useState(false);
+    return type === "password" ? (
+      <div className="relative">
+        <input
+          type={passwordVisible ? "text" : "password"}
+          ref={ref}
+          data-slot="input"
+          className={cn(
+            "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+            "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+            className,
+          )}
+          {...props}
+        ></input>
+        <div
+          className="absolute right-3 top-1/2 -translate-y-1/2"
+          onClick={() => setPasswordVisible((val) => !val)}
+        >
+          {passwordVisible ? (
+            <Eye color="grey" size={15} />
+          ) : (
+            <EyeOff color="grey" size={15} />
+          )}
+        </div>
+      </div>
+    ) : (
       <input
         ref={ref}
         type={type}
