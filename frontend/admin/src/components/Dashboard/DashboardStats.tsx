0b18548017ce14804598104a3435598b891f1b35
Utsarga Manandhar <utsargam44@gmail.com>
2026-01-01 15:58:09 +0545

    FE-feat(dashboard): add dynamic data to chart and stats in dashboard (#238)

--- frontend/admin/src/components/Dashboard/DashboardStats.tsx
+++ frontend/admin/src/components/Dashboard/DashboardStats.tsx
@@ -11,49 +11,40 @@ export const DashboardStats = () => {
   const startDate = formatDate(sevenDaysAgo);
   const endDate = formatDate(new Date());
   const data = useGetOverview(startDate, endDate);
+
+  const totalSales =
+    data?.data?.reduce((sum, val) => sum + val.totalSales, 0) ?? 0;
+  const totalOrders =
+    data?.data?.reduce((sum, val) => sum + val.totalOrders, 0) ?? 0;
+
+  const todaysOrders =
+    data?.data?.find(
+      (val) =>
+        val.date.split("T")[0] === new Date().toISOString().split("T")[0],
+    )?.totalOrders ?? 0;
+  const todaysSales =
+    data?.data?.find(
+      (val) =>
+        val.date.split("T")[0] === new Date().toISOString().split("T")[0],
+    )?.totalSales ?? 0;
   return (
-    <div className="grid grid-cols-2 gap-2.5 lg:grid-cols-3 w-full lg:gap-5 mt-5 ">
+    <div className="grid grid-cols-2 gap-2.5 w-full lg:gap-5 mt-5 ">
       <CardComponent title="Total Sales">
         <>
-          <div className="text-3xl font-semibold mt-5">
-            ${data.data?.[0]?.totalSales}
-          </div>
+          <div className="text-3xl font-semibold mt-5">Rs. {totalSales}</div>
           <div className="mt-3 text-sm text-gray-500 font-medium">
-            Previous 7days{" "}
-            <span className="text-blue-500">
-              (${data.data?.[0]?.totalSales})
-            </span>
+            Today <span className="text-blue-500">(Rs. {todaysSales})</span>
           </div>
         </>
       </CardComponent>
 
       <CardComponent title="Total Orders">
         <>
-          <div className="text-3xl font-semibold mt-5">
-            {data.data?.[0]?.totalOrders}
-          </div>
+          <div className="text-3xl font-semibold mt-5">{totalOrders}</div>
           <div className="mt-3 text-sm text-gray-500 font-medium">
-            Previous 7days{" "}
-            <span className="text-blue-500">{data.data?.[0]?.totalOrders}</span>
+            Today <span className="text-blue-500">{todaysOrders}</span>
           </div>
         </>
-      </CardComponent>
-
-      <CardComponent title="Pending & Canceled">
-        <div className="grid grid-cols-2 gap-2 lg:gap-5 mt-5 mb-1">
-          <div>
-            <div className="text-[14px] lg:text-lg font-semibold">Pending</div>
-            <div className="text-[16px] lg:text-2xl font-semibold text-green-500">
-              509
-            </div>
-          </div>
-          <div>
-            <div className="text-[14px] lg:text-lg font-semibold">Canceled</div>
-            <div className="text-[16px] lg:text-2xl font-semibold text-red-500">
-              94
-            </div>
-          </div>
-        </div>
       </CardComponent>
     </div>
   );
