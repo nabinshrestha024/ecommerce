4f2c79090b3e8d950d4c864fac1b1a3c2c22b688
Aadarsha Khadka <aadar.kdk@gmail.com>
2026-01-08 15:36:00 +0545

    BE-fix(orders): fix variants for admin get orders

--- backend/EcommerceProject/Database/02_Procedures/Orders/spAdminOrders_GetById_Items.sql
+++ backend/EcommerceProject/Database/02_Procedures/Orders/spAdminOrders_GetById_Items.sql
@@ -1,15 +1,35 @@
 USE EcommerceDB;
 GO
 
-CREATE OR ALTER PROCEDURE spAdminOrders_GetById_Items
+CREATE OR ALTER PROCEDURE spAdminOrders_GetById
     @OrderId INT
 AS
 BEGIN
     SET NOCOUNT ON;
 
+    SELECT 
+        o.OrderId,
+        o.UserId,
+        u.FullName AS UserName, 
+        o.OrderDate,
+        o.TotalAmount,
+        o.Status,
+        o.ShippingName,
+        o.ShippingAddress,
+        o.ShippingCity,
+        o.ShippingPhone,
+        o.PaymentMethodId,
+        o.PaymentStatus,
+        o.PaymentGateway,
+        o.Notes
+    FROM Orders o
+    LEFT JOIN Users u ON o.UserId = u.UserId
+    WHERE o.OrderId = @OrderId;
+
     SELECT
         oi.OrderItemId,
         oi.ProductId,
+        oi.VariantId,
         p.Name AS ProductName,
         pi.ImageUrl AS ProductImageUrl,
         p.Description AS ProductDescription,
@@ -18,10 +38,18 @@ BEGIN
         CAST(oi.Quantity * oi.UnitPrice AS DECIMAL(10,2)) AS LineTotal
     FROM OrderItems oi
     INNER JOIN Products p ON p.ProductId = oi.ProductId
-    LEFT JOIN ProductImages pi
-        ON pi.ProductId = oi.ProductId
-       AND pi.IsPrimary = 1
+    LEFT JOIN ProductImages pi ON pi.ProductId = oi.ProductId AND pi.IsPrimary = 1
     WHERE oi.OrderId = @OrderId
     ORDER BY oi.OrderItemId;
+
+    SELECT
+        oi.OrderItemId,
+        pa.Name AS Name,
+        pav.Value AS Value
+    FROM OrderItems oi
+    INNER JOIN VariantAttributeValues vav ON oi.VariantId = vav.VariantId
+    INNER JOIN ProductAttributeValues pav ON vav.AttributeValueId = pav.AttributeValueId
+    INNER JOIN ProductAttributes pa ON pav.AttributeId = pa.AttributeId
+    WHERE oi.OrderId = @OrderId;
 END
 GO
