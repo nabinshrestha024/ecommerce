a179342be7bfacc9eca42eea9f2472a72a1e5a9c
Amardeep Limbu <limbuamardeep18@gmail.com>
2026-01-09 10:30:16 +0545

    FE-fix(image-validation): fix vendor hooks

--- frontend/admin/src/lib/axiosInstance.ts
+++ frontend/admin/src/lib/axiosInstance.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: "http://192.168.80.239/v1/",
   // baseURL: "http://192.168.80.129:5115/v1/",
-  // baseURL: "http://192.168.80.242/v1/",
   baseURL: "http://192.168.80.167:5009/v1/",
   timeout: 10000,
 });
