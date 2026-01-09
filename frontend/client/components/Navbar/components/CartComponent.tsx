f5a84a80590a0c823f6e8d0a540c47c0565b332c
hilubabz <utsargam44@gmail.com>
2026-01-09 10:25:15 +0545

    FE-fix(orders): fix bugs

--- frontend/client/components/Navbar/components/CartComponent.tsx
+++ frontend/client/components/Navbar/components/CartComponent.tsx
@@ -197,7 +197,7 @@ export const CartComponent = () => {
 
                       <div className="mt-auto pt-3 flex items-center justify-between">
                         <span className="text-sm font-bold text-gray-900">
-                          Rs. {val.finalPrice.toLocaleString()}
+                          Rs. {val.finalPrice}
                         </span>
 
                         <div className="flex items-center bg-white border rounded-lg shadow-sm overflow-hidden">
@@ -249,7 +249,7 @@ export const CartComponent = () => {
                 </div>
                 <div className="text-right">
                   <p className="text-2xl font-black text-gray-900 leading-none">
-                    Rs. {totalPrice.toLocaleString()}
+                    Rs. {totalPrice}
                   </p>
                 </div>
               </div>
