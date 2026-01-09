39dfccc72b861453498249f66ac8b2d1d05f1009
Amar <limbuamardeep18@gmail.com>
2026-01-09 10:16:06 +0545

    FE-fix(image-validation): fix vendor hooks

--- frontend/admin/src/hooks/vendor/useCreateVendor.ts
+++ frontend/admin/src/hooks/vendor/useCreateVendor.ts
@@ -1,12 +1,14 @@
 import { postVendor } from "@/services/vendor.services";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 export const useCreateVendor = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: ["putVendor"],
     mutationFn: postVendor,
     onSuccess: () => {
       toast.success("Vendor created successfully");
+      queryClient.invalidateQueries({ queryKey: ["vendor"] });
     },
     onError: () => {
       toast.error("Vendor creation failed");
