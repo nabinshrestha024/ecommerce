b9618ef9deebae0bc96633c806894ae8390e0693
Simran Maharjan <143006616+SimranMhzn@users.noreply.github.com>
2026-01-12 10:52:33 +0545

    FE-fix(admin-product): update import

--- frontend/admin/src/components/Navbar/Navbar.tsx
+++ frontend/admin/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,4 @@
 import { useLocation } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
 import { useGetProfile } from "@/hooks/profile/useGetProfile";
