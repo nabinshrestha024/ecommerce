305c1c7f0908936339cf30b22106617b21663391
hilubabz <utsargam44@gmail.com>
2026-01-12 11:28:29 +0545

    FE-fix(sidebar): fix sidebar click issue

--- frontend/admin/src/components/Sidebar/Sidebar.tsx
+++ frontend/admin/src/components/Sidebar/Sidebar.tsx
@@ -117,7 +117,7 @@ export const Sidebar = () => {
                       >
                         <Link
                           to={value.url}
-                          className="flex items-center gap-3"
+                          className="flex items-center gap-3 w-full"
                         >
                           <Tooltip>
                             <TooltipTrigger>
