71a006650eb2916026b9c5ca68f3f2c183abf998
hilubabz <utsargam44@gmail.com>
2026-01-13 13:41:20 +0545

    FE-fix(dashboard-chart): add last month data to chart

--- frontend/admin/src/components/Dashboard/DashboardStats.tsx
+++ frontend/admin/src/components/Dashboard/DashboardStats.tsx
@@ -1,16 +1,13 @@
 import { useGetOverview } from "@/hooks/report/useGetOverview";
 import { CardComponent } from "./CardComponent";
 
-export const DashboardStats = () => {
-  const formatDate = (date: Date): string => {
-    return date.toISOString().split("T")[0];
-  };
-  const sevenDaysAgo = new Date();
-  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+export const currencyFormatter = new Intl.NumberFormat("en-NP", {
+  style: "currency",
+  currency: "NPR",
+});
 
-  const startDate = formatDate(sevenDaysAgo);
-  const endDate = formatDate(new Date());
-  const data = useGetOverview(startDate, endDate);
+export const DashboardStats = () => {
+  const data = useGetOverview("lastweek");
 
   const totalSales =
     data?.data?.reduce((sum, val) => sum + val.totalSales, 0) ?? 0;
@@ -27,13 +24,19 @@ export const DashboardStats = () => {
       (val) =>
         val.date.split("T")[0] === new Date().toISOString().split("T")[0],
     )?.totalSales ?? 0;
+
   return (
     <div className="grid grid-cols-3 gap-2.5 w-full lg:gap-5 mt-5 ">
       <CardComponent title="Total Sales">
         <>
-          <div className="text-3xl font-semibold mt-2">Rs. {totalSales}</div>
+          <div className="text-3xl font-semibold mt-2">
+            {currencyFormatter.format(totalSales)}
+          </div>
           <div className="text-sm text-gray-500 font-medium mt-2">
-            Today <span className="text-blue-500">(Rs. {todaysSales})</span>
+            Today{" "}
+            <span className="text-blue-500">
+              {currencyFormatter.format(todaysSales)}
+            </span>
           </div>
         </>
       </CardComponent>
