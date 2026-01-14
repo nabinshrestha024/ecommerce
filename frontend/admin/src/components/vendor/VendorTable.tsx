d4d171ebdfc7d2d50e89f1ba114d98a2ec98a81e
Amar <limbuamardeep18@gmail.com>
2026-01-14 11:25:50 +0545

    FE-feat(vendor-search): add data of vendor based on status

--- frontend/admin/src/components/vendor/VendorTable.tsx
+++ frontend/admin/src/components/vendor/VendorTable.tsx
@@ -25,7 +25,9 @@ export const VendorTable = () => {
 
   const vendors = useMemo(() => {
     if (!Array.isArray(data?.data?.items)) return [];
-    return [...data.data.items].sort((a, b) => a.vendorId - b.vendorId);
+    return [...data.data.items]
+      .filter((vendor) => vendor.isActive)
+      .sort((a, b) => a.vendorId - b.vendorId);
   }, [data]);
 
   return (
