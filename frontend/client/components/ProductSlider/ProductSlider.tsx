de3ac03a1f94eac1ff24da8ca26adb92aec4f839
pranawa0718 <pranawakc187@gmail.com>
2026-01-21 15:09:27 +0545

    FE-feat(client-banner): fetch the banner data

--- frontend/client/components/ProductSlider/ProductSlider.tsx
+++ frontend/client/components/ProductSlider/ProductSlider.tsx
@@ -3,14 +3,16 @@
 import Autoplay from "embla-carousel-autoplay";
 import { useMemo } from "react";
 import { Slides } from "./constants/Slides";
-import { Carousel } from "../carousel/Carousel";
 import Link from "next/link";
+import { useFetchBanner } from "@/hooks/banner/useFetchBanner";
+import { Carousel } from "../Carousel/Carousel";
 
 export const ProductSlider = () => {
   const autoplay = useMemo(
     () => Autoplay({ delay: 6000, stopOnInteraction: true }),
     [],
   );
+  const Slides = useFetchBanner();
 
   return (
     <Carousel
@@ -19,31 +21,31 @@ export const ProductSlider = () => {
       nextClassName="absolute right-4 top-1/2 -translate-y-1/2 bg-white text-gray-700 p-2 rounded-full shadow hover:bg-gray-100"
       opts={{ loop: true }}
       plugins={[autoplay]}
-      items={Slides.map((slide) => (
-        <div key={slide.key} className="relative w-full h-[400px]">
-          <img
-            src={slide.src}
-            alt={slide.alt}
-            className="w-full h-full object-cover"
-          />
+      items={
+        Slides.data?.data.map((slide, index) => (
+          <div key={index} className="relative w-full h-[400px]">
+            <img
+              src={slide.imageUrl}
+              alt="image"
+              className="w-full h-full object-cover"
+            />
 
-          {slide.overlay && (
             <div className="absolute inset-0 flex  items-center px-12 bg-black/30">
               <div className="ml-5 text-white max-w-md">
-                <h2 className="text-4xl font-bold">{slide.overlay.title}</h2>
-                <p className="mt-2">{slide.overlay.description}</p>
+                <h2 className="text-4xl font-bold">{slide.title}</h2>
+                <p className="mt-2">{slide.description}</p>
 
                 <Link
-                  href={`/product?categoryId=${slide.overlay.categoryId}`}
+                  href={slide.redirectUrl}
                   className="inline-block mt-4 bg-white text-black px-6 py-2 rounded"
                 >
-                  {slide.overlay.buttonText}
+                  Shop Now
                 </Link>
               </div>
             </div>
-          )}
-        </div>
-      ))}
+          </div>
+        )) ?? []
+      }
     />
   );
 };
