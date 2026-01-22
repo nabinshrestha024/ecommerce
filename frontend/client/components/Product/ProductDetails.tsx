156853aa4c57519cf7a967b76ef599f47cd87786
pranawa0718 <pranawakc187@gmail.com>
2026-01-22 17:04:40 +0545

    FE-fix(wishlist): fix the product card and implement breadcrumbs in product details

--- frontend/client/components/Product/ProductDetails.tsx
+++ frontend/client/components/Product/ProductDetails.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import { useParams } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/ui/button";
 import { useProductDetails } from "@/hooks/product/useProductDetails";
@@ -12,6 +11,14 @@ import { ProductCard } from "./ProductCard";
 import { Card } from "../Card/Card";
 import { ProductReview } from "./Review/ProductReview";
 import { currencyFormatter } from "./ProductDisplay";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/ui/breadcrumb";
+import { useParams } from "next/navigation";
 
 export interface Variant {
   variantId: number;
@@ -124,6 +131,20 @@ const ProductDetails = () => {
 
   return (
     <div className="w-full px-4 sm:px-6 md:px-10 lg:px-20 py-6 md:py-10 flex flex-col gap-5">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/product">Product</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+
+          <BreadcrumbItem>
+            <BreadcrumbLink href={`/product/id/${productItems.data?.slug}`}>
+              {productItems.data?.slug}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <Card className="p-0 ">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 py-2">
           <div className="flex flex-col gap-5 p-2 ">
--- frontend/client/components/Wishlist/Wishlist.tsx
+++ frontend/client/components/Wishlist/Wishlist.tsx
@@ -30,15 +30,13 @@ export const Wishlist = () => {
   const { data } = useFetchWishlist();
 
   return (
-    <div className="w-full">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 w-full gap-6">
-        {data?.items?.map((product: WishlistItem) => (
-          <WishlistProductCard
-            wishlistId={product.wishlistId}
-            productId={product.productId}
-          />
-        ))}
-      </div>
+    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6">
+      {data?.items?.map((product: WishlistItem) => (
+        <WishlistProductCard
+          wishlistId={product.wishlistId}
+          productId={product.productId}
+        />
+      ))}
     </div>
   );
 };
