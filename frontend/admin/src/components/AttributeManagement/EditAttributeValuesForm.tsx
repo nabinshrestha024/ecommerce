90a4ab9d7d901960a078883c1a6439f530bf8f7f
pranawa0718 <pranawakc187@gmail.com>
2026-01-26 09:50:28 +0545

    FE-fix(form): make center align the lable and input field

--- frontend/admin/src/components/AttributeManagement/EditAttributeValuesForm.tsx
+++ frontend/admin/src/components/AttributeManagement/EditAttributeValuesForm.tsx
@@ -57,7 +57,7 @@ export const EditAttributeValueForm = ({
           Edit Attribute Values
         </div>
         <div className="flex-1 overflow-auto mt-5">
-          <div className="flex gap-4 w-full">
+          <div className="flex gap-4 items-center w-full">
             <label className=" font-medium text-gray-700">
               Attribute Values
             </label>
